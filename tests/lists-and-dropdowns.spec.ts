7c08bc21234af9804c8780a93a7383115a442f00
Anna R <anna.radchenko.mail@gmail.com>
2026-04-22 23:00:03 +0200

    changes to address code review comments

--- tests/lists-and-dropdowns.spec.ts
+++ tests/lists-and-dropdowns.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('button', { name: ' OWNERS' }).click();
+  await page.getByRole('button', { name: 'OWNERS' }).click();
   await page.getByRole('link', { name: 'Search' }).click();
   await expect(page.getByRole('heading')).toHaveText('Owners');
 })
@@ -10,26 +10,25 @@ test.beforeEach(async ({ page }) => {
 test('Validate selected pet types from the list', async ({ page }) => {
   await page.getByRole('link', { name: 'George Franklin' }).click();
   await expect(page.locator('.ownerFullName')).toHaveText('George Franklin');
-  const cellOfPetLeo = page.locator('td').filter({ hasText: 'Leo' });
-  await cellOfPetLeo.getByRole('button', { name: 'Edit Pet' }).click();
+  const leoPetSection = page.locator('app-pet-list', { hasText: 'Leo' });
+  await leoPetSection.getByRole('button', { name: 'Edit Pet' }).click(); 
   await expect(page.getByRole('heading')).toHaveText('Pet');
   await expect(page.locator('#owner_name')).toHaveValue('George Franklin');
   const typeField = page.locator('#type1')
   await expect(typeField).toHaveValue('cat');
-  const optionsList = page.locator('#type');
-  const pets = ["cat", "dog", "lizard", "snake", "bird", "hamster"];
-  for (const pet of pets) {
-    await optionsList.selectOption(pet);
-    await expect(typeField).toHaveValue(pet);
-  }
+  const petTypeOptions = await page.locator('#type option').allTextContents();
+  for (const pet of petTypeOptions) {
+  await page.locator('#type').selectOption(pet);
+  await expect(typeField).toHaveValue(pet);
+}
 });
 
 
 test('Validate the pet type update', async ({ page }) => {
   await page.getByRole('link', { name: 'Eduardo Rodriquez' }).click();
-  const cellOfPetRosy = page.locator('td').filter({ hasText: 'Rosy' });
-  await cellOfPetRosy.getByRole('button', { name: 'Edit Pet' }).click();
-   await expect(page.locator('#name')).toHaveValue('Rosy');
+  const rosyPetSection = page.locator('app-pet-list', { hasText: 'Rosy' });
+  await rosyPetSection.getByRole('button', { name: 'Edit Pet' }).click(); 
+  await expect(page.locator('#name')).toHaveValue('Rosy');
   const typeField = page.locator('#type1')
   await expect(typeField).toHaveValue('dog');
   const optionsList = page.locator('#type')
@@ -37,12 +36,12 @@ test('Validate the pet type update', async ({ page }) => {
   await expect(typeField).toHaveValue('bird');
   await expect(optionsList).toHaveValue('bird');
   await page.getByRole('button', { name: 'Update Pet' }).click();
-  await expect(cellOfPetRosy).toContainText('bird');
-  await cellOfPetRosy.getByRole('button', { name: 'Edit Pet' }).click();
+  await expect(rosyPetSection.locator('dd').nth(2)).toHaveText('bird');
+  await rosyPetSection.getByRole('button', { name: 'Edit Pet' }).click();
   await expect(typeField).toHaveValue('bird');
   await optionsList.selectOption('dog');
   await expect(typeField).toHaveValue('dog');
   await expect(optionsList).toHaveValue('dog');
   await page.getByRole('button', { name: 'Update Pet' }).click();
-  await expect(cellOfPetRosy).toContainText('dog');
+  await expect(rosyPetSection.locator('dd').nth(2)).toHaveText('dog');
 });
